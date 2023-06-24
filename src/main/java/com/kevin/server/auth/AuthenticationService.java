c7992f17c3887ab2b03f8f5e5d03d476eb65d7b3
Khefff <heng.kevin19@kit.edu.kh>
2023-06-24 08:32:22 +0700

    first commit

---
+++ src/main/java/com/kevin/server/auth/AuthenticationService.java
@@ -0,0 +1,51 @@
+package com.kevin.server.auth;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.kevin.server.UserRepository;
+import com.kevin.server.config.JwtService;
+import com.kevin.server.user.Role;
+import com.kevin.server.user.User;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepo;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        userRepo.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+        var user = userRepo.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+}
---
+++ src/main/java/com/kevin/server/user/Role.java
@@ -0,0 +1,5 @@
+package com.kevin.server.user;
+
+public enum Role {
+    ADMIN, USER
+}
